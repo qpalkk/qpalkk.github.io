b457b9f657eeb668738e87a2bfe00f731c84d2a8
q <2035675618@qq.com>
2024-05-04 13:25:51 +0800

    新建网站
    原始网站

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+let myVariable;
+myVariable = "李雷";
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼。");
+});
