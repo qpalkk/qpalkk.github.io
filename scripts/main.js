92ecfb9081523883620a06bd35aa10243d14285d
q <2035675618@qq.com>
2024-05-04 13:53:38 +0800

    更改js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+// let myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
 let myVariable;
 myVariable = "李雷";
 document.querySelector("html").addEventListener("click", function () {
